2b494de499c557e46556b845a05cde6300330b4e
Gilbert Wang <gilbert.wang@hp.com>
2023-08-11 15:58:44 +0800

    init project

---
+++ src/test/java/com/wisedevlife/whytalkmessage/WhyTalkMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisedevlife.whytalkmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhyTalkMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
