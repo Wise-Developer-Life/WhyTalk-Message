0fc417a12fc534454f088f4deffaa59684210148
Gilbert Wang <taya87136@gmail.com>
2023-08-23 17:31:52 +0800

    [BE][Message] feat: update message service (support query pagination by chatroom id) (#10)
    * add chatroomId into Message entity
    
    * add findMessagesByChatRoomIdOrderBySendDateTime in message repo
    
    * update message service
    
    * update controller
    
    * update swagger.json

--- src/main/java/com/wisedevlife/whytalkmessage/controller/MessageController.java
+++ src/main/java/com/wisedevlife/whytalkmessage/controller/MessageController.java
@@ -7,15 +7,14 @@ import com.wisedevlife.whytalkmessage.dto.response.MessageResponse;
 import com.wisedevlife.whytalkmessage.dto.response.ReturnResponse;
 import com.wisedevlife.whytalkmessage.entity.Message;
 import com.wisedevlife.whytalkmessage.service.MessageService;
-import java.util.List;
 
-import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Hidden
 @RestController
 @RequestMapping("/message")
 @Tag(name="Message Service API")
@@ -24,27 +23,30 @@ public class MessageController {
 
     private final MessageService messageService;
 
-    @GetMapping
-    public ResponseEntity<ReturnResponse<List<MessageResponse>>> getAllMessages() {
-        List<Message> messages = messageService.getMessages();
-        List<MessageResponse> response = messages.stream().map(MessageResponse::toMessageResponse).toList();
+    @Operation(summary = "Get paged messages from a chat room and sorted by created time in descending order")
+    @GetMapping("/{chatRoomId}")
+    public ResponseEntity<ReturnResponse<Page<MessageResponse>>> getAllMessages(
+            @PathVariable String chatRoomId,
+            @RequestParam("offset") int offset,
+            @RequestParam("limit") int limit
+    ) {
+        // TODO: do not directly expose response which use Spring Data Object Page
+        Page<Message> messages = messageService.getPagedMessagesByChatRoomId(chatRoomId, offset, limit);
+        // TODO: try to use modelMapper to convert between entity and dto
+        Page<MessageResponse> response = messages.map(MessageResponse::toMessageResponse);
         return ResponseHandler.success(response);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ReturnResponse<String>> getMessage(@PathVariable int id) {
-        return ResponseHandler.success("Hello World");
-    }
-
     @PostMapping
-    public ResponseEntity<ReturnResponse<MessageCreateResponse>> postMessage(
-            @RequestBody MessageRequest messageRequest) {
+    @Operation(summary = "Create a message by api, for test only", deprecated = true)
+    public ResponseEntity<ReturnResponse<MessageCreateResponse>> postMessage(@RequestBody MessageRequest messageRequest) {
         messageService.saveMessage(messageRequest);
         MessageCreateResponse response =
                 new MessageCreateResponse(
                         messageRequest.content(),
                         messageRequest.fromUser(),
-                        messageRequest.toUser());
+                        messageRequest.toUser(),
+                        messageRequest.chatRoomId());
         return ResponseHandler.success(response);
     }
 }
