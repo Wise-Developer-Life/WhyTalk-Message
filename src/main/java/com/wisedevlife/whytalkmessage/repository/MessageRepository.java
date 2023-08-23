9c45f2fa1741640d39cf18a50dbb3f31010c92e0
Gilbert Wang <gilbert.wang@hp.com>
2023-08-23 17:26:46 +0800

    add findMessagesByChatRoomIdOrderBySendDateTime in message repo

--- src/main/java/com/wisedevlife/whytalkmessage/repository/MessageRepository.java
+++ src/main/java/com/wisedevlife/whytalkmessage/repository/MessageRepository.java
@@ -1,8 +1,12 @@
 package com.wisedevlife.whytalkmessage.repository;
 
 import com.wisedevlife.whytalkmessage.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, Long> {}
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    Page<Message> findMessagesByChatRoomIdOrderBySendDateTime(String chatRoomId, Pageable pageable);
+}
